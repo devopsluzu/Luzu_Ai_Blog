01d344e31d84f2d2baab1dd15b4aad26182e8f81
Jeevanantham 2002060 <devopsprfecai@gmail.com>
2025-04-11 12:31:26 +0530

    Blog Changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,7 @@ import { AuthContextProvider } from '@context/AuthContext';
 import { GoogleAnalytics } from '@next/third-parties/google'
 
 export const metadata = {
-  title: "Prfec AI",
+  title: "LuzuAI",
   description: "",
 };
 
@@ -14,22 +14,18 @@ export default function RootLayout({ children}) {
   return (
     <html lang="en">
       <head>
-           {/* <script
-        dangerouslySetInnerHTML={{
-          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-KPTVMFRN');`
-        }}
-      /> */}
+      <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GT-MBH8FXW4');`
+          }}
+        />
       </head>
       <body>
         <AuthContextProvider>
           <Navbar/>
           {children}
           <Footer/>
-          {/* <GoogleAnalytics gaId="G-RWEGD69SRV" /> */}
+          <GoogleAnalytics gaId="G-EBZGJH5S9C" />
 
         </AuthContextProvider>
       </body>
